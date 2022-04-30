bbc441d031d28bb2c2cd493eecd1bc659ae7ae7b
kimpoong <sjin0704@gmail.com>
2022-04-30 15:55:44 +0900

    complete

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+require('dotenv').config();
 
 var app = express();
 
@@ -11,7 +13,14 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  var { originalname, mimetype, size } = req.file;
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  });
+})
 
 
 const port = process.env.PORT || 3000;
